4ad8f3887f4e1ee798b839faa2e9d59c3f054db4
khawkins32 <khawkins@netradius.com>
2017-05-11 15:12:11 -0700

    Added db script

---
+++ Resource_DB.sql
@@ -0,0 +1,22 @@
+if not exists (select * from sys.databases where name='CustomResource')
+	CREATE DATABASE CustomResource;
+GO
+USE CustomResource;
+
+if not exists (select * from sysobjects where name='Resource')
+	CREATE TABLE Resource(
+		[resource_id] [int] IDENTITY(1,1) NOT NULL,
+		[resource_type][nvarchar](255),
+		[resource_name] [nvarchar](50) NOT NULL,
+		[culture_code] [nvarchar](50) NULL,
+		[resource_value] [nvarchar](100) NULL,
+	CONSTRAINT [pk_resource] PRIMARY KEY (resource_id));
+GO
+
+SET IDENTITY_INSERT Resource ON;
+ INSERT INTO Resource([resource_id], [resource_type], [resource_name], [culture_code], [resource_value]) VALUES (1, N'CommonTerms', N'helloWorld', null, N'Hello World! Default');
+ INSERT INTO Resource([resource_id], [resource_type], [resource_name], [culture_code], [resource_value]) VALUES (2, N'CommonTerms', N'helloWorld', N'en', N'Hello World! en');
+ INSERT INTO Resource([resource_id], [resource_type], [resource_name], [culture_code], [resource_value]) VALUES (3, N'CommonTerms', N'helloWorld', N'en-US', N'Hello World! en-US');
+ INSERT INTO Resource([resource_id], [resource_type], [resource_name], [culture_code], [resource_value]) VALUES (4, N'CommonTerms', N'helloWorld', N'es', N'Hola Mundo! es');
+ INSERT INTO Resource([resource_id], [resource_type], [resource_name], [culture_code], [resource_value]) VALUES (5, N'CommonTerms', N'helloWorld', N'es-US', N'Hola Mundo! es-US');
+SET IDENTITY_INSERT Resource OFF;
