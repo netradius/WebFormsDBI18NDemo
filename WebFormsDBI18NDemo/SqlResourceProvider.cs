0f782163f26ecc233174c024651f30128df8dd0d
Erik Jensen <ejensen@truemark.io>
2017-05-11 18:25:42 -0700

    Modified provider to use new cache manager

--- WebFormsDBI18NDemo/SqlResourceProvider.cs
+++ WebFormsDBI18NDemo/SqlResourceProvider.cs
@@ -17,7 +17,7 @@ namespace WebFormsDBI18NDemo
         static ILog log = LogManager.GetLogger(typeof(SqlResourceProvider));
         private string _virtualPath;
         private string _className;
-        private IDictionary _resourceCache;
+        //private IDictionary _resourceCache;
         private static object CultureNeutralKey = new object();
 
         public SqlResourceProvider(string virtualPath, string className)
@@ -26,35 +26,35 @@ namespace WebFormsDBI18NDemo
             _className = className;
         }
 
-        private IDictionary GetResourceCache(string cultureName)
-        {
-            if (log.IsDebugEnabled)
-            {
-                log.Debug("Getting resource cache for culture name: " + cultureName);
-            }
-            object cultureKey;
-            if (cultureName != null)
-            {
-                cultureKey = cultureName;
-            }
-            else
-            {
-                cultureKey = CultureNeutralKey;
-            }
+        //private IDictionary GetResourceCache(string cultureName)
+        //{
+        //    if (log.IsDebugEnabled)
+        //    {
+        //        log.Debug("Getting resource cache for culture name: " + cultureName);
+        //    }
+        //    object cultureKey;
+        //    if (cultureName != null)
+        //    {
+        //        cultureKey = cultureName;
+        //    }
+        //    else
+        //    {
+        //        cultureKey = CultureNeutralKey;
+        //    }
 
-            //if (_resourceCache == null)
-            //{
-                _resourceCache = new ListDictionary();
-            //}
-            IDictionary resourceDict = _resourceCache[cultureKey] as IDictionary;
-            if (resourceDict == null)
-            {
-                resourceDict = SqlResourceHelper.GetResources(_virtualPath,
-                              _className, cultureName, false, null);
-                _resourceCache[cultureKey] = resourceDict;
-            }
-            return resourceDict;
-        }
+        //    //if (_resourceCache == null)
+        //    //{
+        //        _resourceCache = new ListDictionary();
+        //    //}
+        //    IDictionary resourceDict = _resourceCache[cultureKey] as IDictionary;
+        //    if (resourceDict == null)
+        //    {
+        //        resourceDict = SqlResourceHelper.GetResources(_virtualPath,
+        //                      _className, cultureName, false, null);
+        //        _resourceCache[cultureKey] = resourceDict;
+        //    }
+        //    return resourceDict;
+        //}
 
         object IResourceProvider.GetObject(string resourceKey, CultureInfo culture)
         {
@@ -73,17 +73,17 @@ namespace WebFormsDBI18NDemo
                 log.Debug("Getting resource provider for resource key " + resourceKey + " and culture " + cultureName);
             }
 
-            object value = GetResourceCache(cultureName)[resourceKey];
+            object value = ResourceCacheManager.Instance.GetResourceCache(_virtualPath, _className, cultureName)[resourceKey];
             // if the value is not found and the culture is language-country, then try just language
             if (value == null)
             {
                 string[] cultureInfo = cultureName.Split('-');
-                value = GetResourceCache(cultureInfo[0])[resourceKey];
+                value = ResourceCacheManager.Instance.GetResourceCache(_virtualPath, _className, cultureInfo[0])[resourceKey];
             }
             // if the value is not found, try no culture
             if (value == null)
             {
-                value = GetResourceCache(null)[resourceKey];
+                value = ResourceCacheManager.Instance.GetResourceCache(_virtualPath, _className, null)[resourceKey];
             }
             return value;
         }
@@ -98,7 +98,7 @@ namespace WebFormsDBI18NDemo
                 {
                     cultureName = currentUICulture.Name;
                 }
-                return new SqlResourceReader(GetResourceCache(cultureName));
+                return new SqlResourceReader(ResourceCacheManager.Instance.GetResourceCache(_virtualPath, _className, cultureName));
             }
         }
     }
