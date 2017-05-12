8eab26e3fc27fd238f5b1c9c8c998eff6a8d419b
khawkins32 <khawkins@netradius.com>
2017-05-11 18:40:28 -0700

    Modified code convention for private attributes.

--- WebFormsDBI18NDemo/SqlResourceProvider.cs
+++ WebFormsDBI18NDemo/SqlResourceProvider.cs
@@ -15,15 +15,15 @@ namespace WebFormsDBI18NDemo
     public sealed class SqlResourceProvider : IResourceProvider
     {
         static ILog log = LogManager.GetLogger(typeof(SqlResourceProvider));
-        private string _virtualPath;
-        private string _className;
+        private string virtualPath;
+        private string className;
         //private IDictionary _resourceCache;
         private static object CultureNeutralKey = new object();
 
         public SqlResourceProvider(string virtualPath, string className)
         {
-            _virtualPath = virtualPath;
-            _className = className;
+            this.virtualPath = virtualPath;
+            this.className = className;
         }
 
         //private IDictionary GetResourceCache(string cultureName)
@@ -73,17 +73,17 @@ namespace WebFormsDBI18NDemo
                 log.Debug("Getting resource provider for resource key " + resourceKey + " and culture " + cultureName);
             }
 
-            object value = ResourceCacheManager.Instance.GetResourceCache(_virtualPath, _className, cultureName)[resourceKey];
+            object value = ResourceCacheManager.Instance.GetResourceCache(this.virtualPath, this.className, cultureName)[resourceKey];
             // if the value is not found and the culture is language-country, then try just language
             if (value == null)
             {
                 string[] cultureInfo = cultureName.Split('-');
-                value = ResourceCacheManager.Instance.GetResourceCache(_virtualPath, _className, cultureInfo[0])[resourceKey];
+                value = ResourceCacheManager.Instance.GetResourceCache(this.virtualPath, this.className, cultureInfo[0])[resourceKey];
             }
             // if the value is not found, try no culture
             if (value == null)
             {
-                value = ResourceCacheManager.Instance.GetResourceCache(_virtualPath, _className, null)[resourceKey];
+                value = ResourceCacheManager.Instance.GetResourceCache(this.virtualPath, this.className, null)[resourceKey];
             }
             return value;
         }
@@ -98,7 +98,7 @@ namespace WebFormsDBI18NDemo
                 {
                     cultureName = currentUICulture.Name;
                 }
-                return new SqlResourceReader(ResourceCacheManager.Instance.GetResourceCache(_virtualPath, _className, cultureName));
+                return new SqlResourceReader(ResourceCacheManager.Instance.GetResourceCache(this.virtualPath, this.className, cultureName));
             }
         }
     }
